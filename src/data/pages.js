32f7e4419c0ce52806707b1e8b9cb3da2401fb01
samfromaway@gmail.com <email@example.com>
2020-09-18 13:37:48 -0500

    text change

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,61 +1,61 @@
-import ReactLogo from "../images/icons/react-logo.svg";
-import FirebaseLogo from "../images/icons/firebase.png";
-import Design from "../images/icons/design.svg";
+import ReactLogo from '../images/icons/react-logo.svg';
+import FirebaseLogo from '../images/icons/firebase.png';
+import Design from '../images/icons/design.svg';
 
-import Home from "../images/icons/home.svg";
-import PageView from "../images/icons/pageview.svg";
-import People from "../images/icons/people.svg";
-import SettingsInput from "../images/icons/settings-input.svg";
-import Verified from "../images/icons/verified.svg";
-import CallMissed from "../images/icons/call-missed.svg";
+import Home from '../images/icons/home.svg';
+import PageView from '../images/icons/pageview.svg';
+import People from '../images/icons/people.svg';
+import SettingsInput from '../images/icons/settings-input.svg';
+import Verified from '../images/icons/verified.svg';
+import CallMissed from '../images/icons/call-missed.svg';
 
-const react = { title: "React", logo: ReactLogo };
-const firebase = { title: "Firebase", logo: FirebaseLogo };
-const design = { title: "Design", logo: Design };
+const react = { title: 'React', logo: ReactLogo };
+const firebase = { title: 'Firebase', logo: FirebaseLogo };
+const design = { title: 'Design', logo: Design };
 
 export const info = [
   {
-    title: "Home",
-    path: "/",
+    title: 'Home',
+    path: '/',
     icon: Home,
   },
   {
-    title: "About",
-    path: "/about",
+    title: 'About',
+    path: '/about',
     icon: People,
   },
 ];
 
 export const resources = [
   {
-    title: "ContextAPI",
+    title: 'ContextAPI',
     desc:
-      "Organize your state in a modular way while making it available globally in your app.",
-    path: "/context-api",
+      'Organize your state in a modular way while making it available globally in your app.',
+    path: '/context-api',
     categories: react,
     icon: SettingsInput,
   },
   {
-    title: "Validation Firebase",
+    title: 'Firestore Rules',
     desc:
-      "Keep your database security with the security rules of Firebase. Implement validation checks on the client and database side.",
-    path: "/validation-firebase-web",
+      'Keep your database security with the security rules of Firebase. Implement validation checks on the client and database side.',
+    path: '/validation-firebase-web',
     categories: firebase,
     icon: Verified,
   },
   {
-    title: "React State Hooks",
+    title: 'React State Hooks',
     desc:
-      "Make it easier to create your useState hooks. Add the name, and get the props and hooks automatically.",
-    path: "/react-state-hooks",
+      'Make it easier to create your useState hooks. Add the name, and get the props and hooks automatically.',
+    path: '/react-state-hooks',
     categories: react,
     icon: CallMissed,
   },
   {
-    title: "Design Resources",
+    title: 'Design Resources',
     desc:
-      "Select your resource from over 400 free services that will help you design your next project.",
-    path: "/design-resources",
+      'Select your resource from over 400 free services that will help you design your next project.',
+    path: '/design-resources',
     categories: design,
     icon: PageView,
   },
